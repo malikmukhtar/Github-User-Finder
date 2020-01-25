02e7cb98809a13e9dddd87d887522ca4d1f0ce0b
Malik Mukhtar <malikmukhtar63@gmail.com>
2020-01-25 17:24:15 +0100

    t
    t
    i

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <div>
+            <h>Not Found</h>
+            <p className='lead'>The page you are looking for does not exist...</p>
+        </div>
+    )
+}
+
+export default NotFound;
